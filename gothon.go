da2a029b905cd5d634fdc08a147cff13c880908e
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2015-06-24 20:18:18 +0200

    Clean up gothon.go

--- gothon.go
+++ gothon.go
@@ -4,18 +4,13 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"path"
-
-	"github.com/flowlo/gothon"
-	//	"encoding/json"
 )
 
 var (
-	verbose = flag.Bool("v", false, "verbose. if set, gothon will tell you what is going on.")
-	debug   = flag.Bool("d", true, "debug. if set, gothon will tell precicesly what it is doing. implies verbose")
+	debug = flag.Bool("d", true, "debug. if set, gothon will tell precicesly what it is doing.")
 )
 
 func main() {
@@ -27,69 +22,52 @@ func main() {
 
 	flag.Parse()
 
-	if *debug {
-		*verbose = true
-	}
-
 	if len(flag.Args()) != 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	target := flag.Args()[0]
-
-	if !path.IsAbs(target) {
-		pwd, _ := os.Getwd()
-		target = path.Join(pwd, target)
-	}
-
-	//fmt.Println(target)
-
-	if path.Ext(target) == ".py" {
-		_, err := exec.LookPath("python3.4")
-		if err != nil {
-			log.Fatalf("If you want gothon to compile \"%s\", please install Python 3.4 (Could not find \"python3.4\")", path.Base(target))
-		}
-		//if *debug { log.Print("Found Python 3.4") }
-
-		cmd := exec.Command("python3.4", "-m", "compileall", target)
-		output, err := cmd.CombinedOutput()
-
-		if len(output) > 0 {
-			fmt.Print(string(output))
-		}
-
-		if err != nil {
-			os.Exit(1)
-		}
-
-		//if *verbose { log.Print("Compilation finished") }
-		base := path.Base(target)
-		target = path.Join(path.Dir(target), "__pycache__", base[:len(base)-2]+"cpython-34.pyc")
-	}
-
-	var file *os.File
-
-	file, err := os.Open(target)
+	file, err := resolve(flag.Args()[0])
+	defer file.Close()
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	//if *verbose { log.Print("Using file \"", target, "\"") }
 
-	module := &gothon.Module{}
-	reader := gothon.Reader{*bufio.NewReader(file), *module}
+	module := &Module{}
+	reader := Reader{*bufio.NewReader(file), *module}
 
 	module.Read(&reader, 0)
 
-	fmt.Println()
-
-	/*	dump, err := json.Marshal(module)
-		if err != nil { log.Fatal(err) }
-		log.Printf("%s", dump) */
-
-	file.Close()
-
-	frame := gothon.NewFrame(module.Code)
+	frame := NewFrame(module.Code)
 	frame.Execute()
+}
+
+func resolve(target string) (file *os.File, err error) {
+	if path.Ext(target) == ".pyc" {
+		file, err = os.Open(target)
+		return
+	}
+
+	if _, err := exec.LookPath("python3.4"); err != nil {
+		err = fmt.Errorf("gothon: python3.4 needed for compilation")
+		return nil, err
+	}
+
+	cmd := exec.Command("python3.4", "-m", "compileall", "-l", target)
+	output, err := cmd.CombinedOutput()
+
+	//if len(output) > 0 {
+	fmt.Print(string(output))
+	//}
+
+	if err != nil {
+		return
+	}
+
+	base := path.Base(target)
+	target = path.Join(path.Dir(target), "__pycache__", base[:len(base)-2]+"cpython-34.pyc")
+	file, err = os.Open(target)
+	return
 }
